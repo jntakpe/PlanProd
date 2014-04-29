21cf0e3a0442eacd3d1d270d974b76adb90d428b
jntakpe <jocelyn.ntakpe@sopragroup.com>
2014-04-29 17:21:32 +0200

    bower et écran login

--- src/main/java/com/github/jntakpe/pp/PP.java
+++ src/main/java/com/github/jntakpe/pp/PP.java
@@ -2,8 +2,10 @@ package com.github.jntakpe.pp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 /**
  * Classe mère de l'application
@@ -17,6 +19,14 @@ public class PP {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(PP.class, args);
+    }
+
+    @Bean
+    public ReloadableResourceBundleMessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasenames("classpath:/messages/gui-i18n");
+        messageSource.setCacheSeconds(10);
+        return messageSource;
     }
 
 }
---
+++ src/main/java/com/github/jntakpe/pp/config/SecurityConfiguration.java
@@ -0,0 +1,48 @@
+package com.github.jntakpe.pp.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Configuration de Spring security
+ *
+ * @author jntakpe
+ */
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("admin").roles("ADMIN").and()
+                .withUser("user").password("user").roles("USER");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/bower_components/**")
+                .antMatchers("/css/**")
+                .antMatchers("/img/**")
+                .antMatchers("/js/**");
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.
+                formLogin().loginProcessingUrl("/login-process").loginPage("/login").permitAll().and().
+                logout().logoutUrl("/logout").deleteCookies("JSESSIONID").permitAll().and().
+                authorizeRequests().
+                antMatchers("/**").authenticated().
+                antMatchers("/admin/**").hasAuthority(Authorities.ROLE_ADMIN.name());
+    }
+
+    private static enum Authorities {
+        ROLE_ADMIN,
+        ROLE_USER
+    }
+}
